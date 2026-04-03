6f48c49c9dbf8e139515854e0403ceb70eab7ed3
Maitreyi Joshi <joshimaitreyiii27@gmail.com>
2026-04-03 23:55:38 +0530

    fix server app

---
+++ server/app.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from env import EmailEnv
+
+app = FastAPI()
+
+env = EmailEnv()
+
+def main():
+    return app
+
+@app.post("/reset")
+def reset():
+    state = env.reset()
+    return {"state": state.tolist()}
+
+@app.post("/step")
+def step(action: int = 0):
+    next_state, reward, done, info = env.step(action)
+    return {
+        "state": next_state.tolist(),
+        "reward": reward,
+        "done": done
+    }
